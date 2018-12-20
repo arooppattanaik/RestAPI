4cfcbeb19a0934a06e3f8c2bd44e428313a57882
arooppattanaik <arooppattanaik@gmail.com>
2018-12-20 03:59:02 -0500

    AP: Consume RestAPI using JSON

---
+++ FinalProject/FinalProject/weather.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="weather.aspx.cs" Inherits="FinalProject.weather" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+   <head runat="server">
+      <link rel="stylesheet" href="/css/bootstrap.css"/>
+      <link rel="stylesheet" href="/css/font-awesome.min.css"/>
+      <link rel="stylesheet" href="/css/weather_lyric.css"/>
+      <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"/>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>    
+    <script>
+    $( function(){
+        $("#CityName").autocomplete({
+           source: "weatherAutoComplete.aspx",
+          minLength : 2
+        });
+    });
+  
+  </script>
+      <title>Weather Report</title>
+
+   </head>
+   <body id="weatherImg">
+      <form id="form1" runat="server">
+         <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+            <div class="container">
+               <a href="#" class="navbar-brand">Weather Data</a>
+               <button class="navbar-toggler" data-toggle="collapse" data-target="#navNavbar">
+               <span class="navbar-toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse" id="navNavbar">
+                  <ul class="navbar-nav ml-auto">
+                     <li class="nav-item active"><a href="weather.aspx" class="nav-link">Weather</a></li>
+                     <li class="nav-item"><a href="lyrics.aspx" class="nav-link">Lyrics</a></li>
+                     <li class="nav-item"><a href="BandDetail.aspx" class="nav-link">Band Details JSON</a></li>
+                     
+                  </ul>
+               </div>
+            </div>
+         </nav>
+         <section class="py-3">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-6">
+                     <h4 class="text-primary"><strong>Current Day Weather Report by City</strong></h4>
+                     <div class="form-group">
+                        <input id="CityName" type="text" class="form-control" placeholder="E.g.: London,UK" runat="server"/>
+                     </div>
+                     <asp:Button  runat="server" class="btn btn-primary" Text="Get Current Day Weather Report" OnClick="BtnClk"/>
+                     <br />
+                     <asp:Label ID="Weather_Info" runat="server" Text=""></asp:Label>
+                  </div>
+               </div>
+            </div>
+         </section>
+      </form>
+   </body>
+</html>
---
+++ FinalProject/FinalProject/weather.aspx.cs
@@ -0,0 +1,81 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Final_Project;
+using Newtonsoft.Json;
+
+namespace FinalProject
+{
+    public partial class weather : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void BtnClk(object sender, EventArgs e)
+        {
+            WeatherDetails();
+        }
+        public void WeatherDetails()
+        {
+            Weather_Info.Text = "";
+            string cityName = CityName.Value;
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=e6b20cdb29dd3e1271611dc84f374003";
+            Console.WriteLine("URL is " + url);
+            Uri uri = new Uri(@url);
+            int statusNumber;
+            HttpWebResponse res = null;
+            try
+            {
+                WebRequest webRequest = WebRequest.Create(uri);
+                res = (HttpWebResponse)webRequest.GetResponse();
+                statusNumber = (int)res.StatusCode;
+            }
+            catch (WebException we)
+            {
+                statusNumber = (int)we.Status;
+                if (statusNumber == 404)
+                {
+                    Weather_Info.Text = "<br/>" + "<font color=" + "white" + ">" + "<p>We could not recognize the city: <strong>" + cityName +
+                        "</strong>. Oops! Something went wrong, please try again. </p>" + "</font>";
+                }
+            }
+
+            if (res == null)
+            {
+                Weather_Info.Text = "<br/>" + "<font color=" + "white" + ">" + "<p>We could not recognize the city: <strong>" + cityName +
+                    "</strong>. While we are continously trying to upgrade, we encourage you to contact us to include this city. </p>" + "</font>";
+            }
+            else
+            {
+                StreamReader streamReader = new StreamReader(res.GetResponseStream());
+                String responseData = streamReader.ReadToEnd();
+
+                RootObject result_Collection = JsonConvert.DeserializeObject<RootObject>(responseData);
+
+
+                if (result_Collection == null)
+                {
+                    Weather_Info.Text = "<br/>" + "<font color=" + "white" + ">" + "<p>No search results found for <strong>" + cityName +
+                        "</strong>. This movie seems to be not so famous. </p>" + "</font>";
+                }
+                else
+                {
+
+                    Weather_Info.Text = "<br/>" + "<font color=" + "white" + ">" + "<strong> Longitude: </strong>" + result_Collection.Coord.Lon + "<br/><strong> Lattitude: </strong>" + result_Collection.Coord.Lat
+                        + " <br/><strong>Temperature: </strong>" + result_Collection.Main.Temp + " F <br/><strong>Pressure: </strong>" + result_Collection.Main.Pressure + " P<br/><strong>Humidity: </strong>"
+                        + result_Collection.Main.Humidity + " %<br/><strong>Minimum Temperature: </strong>" + result_Collection.Main.Temp_min + " F<br/><strong> Maximum Temperature: </strong> " + result_Collection.Main.Temp_max +
+                        " F<br/><strong> Speed of the wind: </strong> " + result_Collection.Wind.Speed + " km/hr<br/><strong> Deg of the wind: </strong> " + result_Collection.Wind.Deg + "</font>";
+
+                }
+            }
+
+        }
+    }
+}
